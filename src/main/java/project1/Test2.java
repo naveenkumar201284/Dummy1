289e3007aa928eac6a3e8e28c0d9ff7ea87bc3e8
naveenkumar <naveen.kumar@profinch.com>
2023-10-01 22:46:04 +0530

    new code commited test2

---
+++ src/main/java/project1/Test2.java
@@ -0,0 +1,18 @@
+package project1;
+
+public class Test2 {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a =10;
+		int b=20;
+		
+		if(a>b) {
+			System.out.println("The record get failed");
+		}
+
+	}
+
+}
